37c5221f9af3e6e90cc48a41ba0d8dfe1194db0d
Eric LIN <eric.lin@telecomnancy.eu>
2023-12-04 15:29:41 +0100

    [Update] Clean unused imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,11 @@
 package org.example;
 
-import MongoDB.MongoDBDao;
-import Protein.ProteinData;
-import com.google.gson.Gson;
-import org.bson.Document;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class Main {
 
     public static void main(String[] args) {
-        MongoDBDao mongoDBDao = MongoDBDao.getInstance();
         // Testing queries
         /*
+        MongoDBDao mongoDBDao = MongoDBDao.getInstance();
         List<ProteinData> query = mongoDBDao.getProteinByEC("1.14.14.1");
         List<ProteinData> query2 = mongoDBDao.getProteinByName("transport");
         List<ProteinData> query3 = mongoDBDao.getProteinByInterPro("IPR011009");
