a6b9069c41acdcf0505167a431ddf3a7a695c1de
Krabumb <krabumb.gaming@gmail.com>
2023-12-08 15:04:22 +0100

    Added ProteinController

---
+++ src/main/java/fr/lesbg/Protein/ProteinController.java
@@ -0,0 +1,42 @@
+package fr.lesbg.Protein;
+
+import fr.lesbg.neo4j.Connector;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController("/protein")
+public class ProteinController {
+
+    private final Connector connector = Connector.getInstance();
+
+    @GetMapping("/entry/{entry}")
+    public ProteinData getProteinFromEntry(@PathVariable String entry) {
+        return connector.getProteinFromEntry(entry);
+    }
+
+    @GetMapping("/entryName/{entryName}")
+    public ProteinData getProteinFromEntryName(@PathVariable String entryName) {
+        return connector.getProteinFromName(entryName);
+    }
+
+    @GetMapping("/description/{description}")
+    public ProteinData getProteinFromDescription(@PathVariable String description) {
+        return connector.getProteinFromDescription(description);
+    }
+
+    @GetMapping("/neighbours/{entry}")
+    public List<ProteinData> getProteinNeighbours(@PathVariable String entry) {
+        ProteinData proteinData = connector.getProteinFromEntry(entry);
+        return connector.getNeighborsFromProtein(proteinData);
+    }
+
+    @GetMapping("/neighboursAndNeighboursOfNeighbours/{entry}")
+    public Map<ProteinData, List<ProteinData>> getProteinNeighboursAndNeighboursOfNeighbours(@PathVariable String entry) {
+        ProteinData proteinData = connector.getProteinFromEntry(entry);
+        return connector.getNeighborsAndNeigborsofNeighborsFromProtein(proteinData);
+    }
+}
