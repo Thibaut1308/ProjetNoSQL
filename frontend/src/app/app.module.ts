4e04f892a1dc5cacea38a6ee21bd518144e3a210
Eric LIN <eric.lin@telecomnancy.eu>
2023-12-11 00:29:08 +0100

    [Update] Front un peu moins moche

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatRadioModule} from "@angular/material/radio";
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import {MatRadioModule} from "@angular/material/radio";
     FormsModule,
     MatRadioModule,
     ReactiveFormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
